656f259756e19506bd8610f9740a4f4bc4b62f76
Jan-Niklas Wortmann <jan-niklas.wortmann@jetbrains.com>
2024-03-07 08:05:41 -0600

    initialize template

---
+++ components/socials.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+type Socials = { twitter?: string, linkedin?: {name: string, path: string}, github?: string }
+  const props = defineProps<Socials>()
+
+</script>
+<template>
+  <div class="flex items-center justify-between" v-if="!animated">
+    <div v-if="props.twitter">
+      <a :href="`https://twitter.com/${props.twitter}`"><mdi:twitter /> @{{props.twitter}}</a>
+    </div>
+    <div v-if="props.linkedin">
+      <a :href="`https://www.linkedin.com/in/${props.linkedin.path}`"><mdi:linkedin /> {{props.linkedin.name}}</a>
+    </div>
+    <div v-if="props.github">
+      <a :href="`https://github.com/${props.twitter}`"><mdi:github /> {{props.github}}</a>
+    </div>
+  </div>
+</template>
---
+++ setup/shiki.ts
@@ -0,0 +1,8 @@
+import { defineShikiSetup } from '@slidev/types'
+import {DarkRaw} from "jetbrains-ide-themes";
+
+export default defineShikiSetup(() => {
+  return {
+    theme: DarkRaw,
+  }
+})
