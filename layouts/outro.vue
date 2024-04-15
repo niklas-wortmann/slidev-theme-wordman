783da2302a2d0fa9e35c2516ad69c6ce357ca277
Jan-Niklas Wortmann <jan-niklas.wortmann@jetbrains.com>
2024-04-15 10:54:03 -0500

    add outro and intro slide

---
+++ layouts/outro.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>{{props.title}}</h1>
+  <div class="flex w-full items-center justify-center">
+    <a :href="props.url">
+      <qrcode :value="props.url" class="mb-2 mt-2"> </qrcode>
+      <span>{{ props.url }}</span>
+    </a>
+  </div>
+</template>
+<script setup lang='ts'>
+type Props = { url: string, title: string }
+const props = withDefaults(defineProps<Props>(), {title: "Thanks for Listening 🙏"})
+</script>
