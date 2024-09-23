428e963de548c6f98dee9a86aed6e7c5575f06ab
Jan-Niklas Wortmann <jan-niklas.wortmann@jetbrains.com>
2024-09-23 12:12:38 -0500

    fix footer and bump version

--- components/socials.vue
+++ components/socials.vue
@@ -2,20 +2,18 @@
 
 import { ref } from 'vue'
 import { useMotions } from "@vueuse/motion";
-
-type Socials = { twitter: string, linkedin: {name: string, path: string}, github: string, animated?: boolean, animationTime?: number, homepage: string }
+import {  computed } from 'vue'
+type Socials = { twitter: string, linkedin: {name: string, path: string}, github: string, animated?: boolean, homepage: string }
 const props = defineProps<Socials>()
 
-const initial = {y:  100, opacity: 0};
-const enter = {y: 0, opacity: 1 };
-const timer = ref(0);
-setInterval(x => {
-  if(timer.value >= 3) {
-    timer.value = 0
-  } else {
-    timer.value++
-  }
-}, props.animationTime ?? 30000)
+import {onSlideEnter, useNav, useSlideContext} from '@slidev/client'
+
+
+// Add a reactive index to track the current social link
+const { currentSlideNo } = useNav();
+const currentIndex = computed(() => {
+  return (currentSlideNo.value-2) % 4
+})
 
 
 </script>
@@ -34,17 +32,17 @@ setInterval(x => {
       <a :href="`${props.homepage}`"><mdi:home-circle-outline /> {{props.homepage}}</a>
     </div>
   </div>
-  <div v-else>
-      <div v-if="props.twitter && timer === 0" v-motion :initial="initial" :enter="enter">
+  <div v-else-if="$slidev.nav.currentPage !== 1">
+      <div v-if="props.twitter && currentIndex === 0">
         <a :href="`https://twitter.com/${props.twitter}`"><mdi:twitter /> @{{props.twitter}}</a>
       </div>
-      <div v-if="props.linkedin && timer === 1" v-motion :initial="initial" :enter="enter">
+      <div v-if="props.linkedin && currentIndex === 1">
         <a :href="`https://www.linkedin.com/in/${props.linkedin.path}`"><mdi:linkedin /> {{props.linkedin.name}}</a>
       </div>
-      <div v-if="props.github && timer === 2" v-motion :initial="initial" :enter="enter">
+      <div v-if="props.github && currentIndex === 2">
         <a :href="`https://github.com/${props.twitter}`"><mdi:github /> {{props.github}}</a>
       </div>
-      <div v-if="props.homepage && timer === 3" v-motion :initial="initial" :enter="enter">
+      <div v-if="props.homepage && currentIndex === 3">
         <a :href="`${props.homepage}`"><mdi:home-circle-outline /> {{props.homepage}}</a>
       </div>
   </div>
---
+++ global-bottom.vue
@@ -0,0 +1,7 @@
+
+<template>
+  <footer class="absolute bottom-0 left-0 right-0 p-2 z-20">
+    <Socials
+        twitter="niklas_wortmann" :linkedin="{name: 'Jan-Niklas Wortmann', path: 'jan-niklas-wortmann'}" github="niklas-wortmann" homepage="https://wordman.dev" animated> </Socials>
+  </footer>
+</template>
